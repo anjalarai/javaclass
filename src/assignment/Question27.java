74b2f24c00f24bbfc81859b8f8076438332c752f
anjala <anjalarai05@gmail.com>
2021-03-12 20:50:05 -0600

    added

--- src/assignment/Question27.java
+++ src/assignment/Question27.java
@@ -1,4 +1,36 @@
 package assignment;
-
+import java.util.Scanner;
 public class Question27 {
+    private static void sumEachNumber(int number) {
+        int sum = 0;
+        int temp;
+        while (number % 10 != 0) {
+            temp = number % 10;
+            sum = sum + temp;
+            number = number / 10;
+        }
+        System.out.println("Sum is: " + sum);
+    }
+
+    private static void productEachNumber(int number) {
+        int product = 1;
+        int temp;
+        while (number % 10 != 0) {
+            temp = number % 10;
+            product = product * temp;
+            number = number / 10;
+        }
+        System.out.println("Product is: " + product);
+    }
+
+    public static void main(String[] args) {
+        int num;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        num = input.nextInt();
+
+        sumEachNumber(num);
+        productEachNumber(num);
+    }
 }
--- src/assignment/Question37.java
+++ src/assignment/Question37.java
@@ -1,22 +1,97 @@
 package assignment;
 import java.util.Scanner;
 public class Question37 {
-    public static void main(String[] args) {
-        final int ROWS =3;
-        final int COLUMNS=3;
 
-        int [][]arr = new int [ROWS][COLUMNS];
+    public void acceptArray(int arrA[][], int arrB[][], int rows, int columns, Scanner input){
+        System.out.println("Enter for Array A:");
+                for(int i = 0; i<rows; i++)
+                {
+                    for(int j = 0; j<columns; j++)
+                    {
+                        System.out.println("Enter A: ");
+                        arrA[i][j] = input.nextInt();
+                    }
 
-        Scanner input = new Scanner (System.in);
-
-        for (int i =0; i<ROWS; i++){
-            for (int j=0; j<COLUMNS; j++) {
-                System.out.println("Enter:");
-                arr[i][j]=input.nextInt();
+                }
+                System.out.println("Enter for Array B:");
+                for(int i = 0; i<rows; i++)
+                {
+                    for(int j = 0; j<columns; j++)
+                    {
+                        System.out.println("Enter B: ");
+                        arrB[i][j] = input.nextInt();
+                    }
+                }
             }
 
+            // after accepting the user input, print the arrays in 3X3 dimensional form
+            public void printArray(int arrA[][], int arrB[][], int rows, int columns)
+            {
+                System.out.println("Array A: ");
+                for(int  i =0; i<rows; i++)
+                {
+                    for(int j = 0; j<columns; j++)
+                    {
+                        System.out.print(arrA[i][j]+" ");
+                    }
+                    System.out.println();
+                }
+
+                System.out.println("Array B: ");
+                for(int  i =0; i<rows; i++)
+                {
+                    for(int j = 0; j<columns; j++)
+                    {
+                        System.out.print(arrB[i][j]+" ");
+                    }
+                    System.out.println();
+                }
+            }
+
+            // to carry out each element sum and print/store them in array form
+            public void additionArray(int arrA[][], int arrB[][], int sum[][]){
+                System.out.println("The sum is: ");
+                for (int i=0; i<3; i++)
+                {
+                    for(int j = 0; j<3; j++)
+                    {
+                        sum[i][j]=arrA[i][j]+arrB[i][j];
+                    }
+                }
+
+                System.out.println("Sum of Array is: ");
+                for(int  i =0; i<3; i++)
+                {
+                    for(int j = 0; j<3; j++)
+                    {
+                        System.out.print(sum[i][j]+" ");
+                    }
+                    System.out.println();
+                }
+            }
+
+            // main method
+            public static void main(String[] args) {
+
+                // definition for arrays
+                final int ROWS_A = 3;
+                final int COLUMNS_A = 3;
+                int [][] arrA= new int[ROWS_A][COLUMNS_A];
+
+                int [][] arrB = new int[3][3];
+
+                int [][] sum = new int[3][3];
+
+                //Scanner for input access
+                Scanner input = new Scanner(System.in);
+
+                //creating object to call the function
+                Question37 obj = new Question37();
+                // first accept the two 3X3 array then print and then sum and print the array
+                obj.acceptArray(arrA,arrB,ROWS_A,COLUMNS_A, input);
+                obj.printArray(arrA, arrB, ROWS_A,COLUMNS_A);
+                obj.additionArray(arrA, arrB, sum);
             }
         }
 
-    }
 
