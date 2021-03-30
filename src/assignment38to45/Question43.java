d796ac1c1fe96fef8e8d30e12454f14c2fc03f22
anjala <anjalarai05@gmail.com>
2021-03-29 20:36:18 -0500

    43 added

---
+++ src/assignment38to45/Question43.java
@@ -0,0 +1,21 @@
+package assignment38to45;
+
+public class Question43 {
+
+    public static void main(String[] args)
+    {
+        try {
+            int number[] = new int[10];
+            number[10] = 30 / 0;
+        }
+        catch (ArithmeticException e) {
+            System.out.println(
+                    "Zero cannot divide any number");
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(
+                    "Index out of size of the array");
+        }
+    }
+
+}
